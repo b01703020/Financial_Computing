77df56ad136672833b74dbb77d57810d1ddae67c
b01703020 <73329275+b01703020@users.noreply.github.com>
2022-01-29 13:53:54 -0500

    Add files via upload

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by wu yen sun on 2022/1/28.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{ //display message
+    cout << "Hello World!" << endl;
+    //take input from keyboard
+    double price = 0.0;
+    cin >> price;
+    cout << "price = " << price << endl;
+    char x = '\0'; //null character
+    cin >> x;
+    cout << "x = " << x <<endl;
+    return 0;
+}
